772b5bc33a17663670db7aa1f2b138d3adb733a4
Z <asterixandisaac@gmail.com>
2019-11-30 00:38:45 -0700

    fixed merged conflicts

--- src/pages/nice.js
+++ src/pages/nice.js
@@ -34,25 +34,67 @@ const Title2 = styled(TitleSecondary)`
 const AnimatedContent = ({ callback = () => {}, onClick }) => {
   const [show, setShow] = useState(true)
   const [destination, setDestination] = useState("/")
-  const transitions = useTransition(show, null, {
-    from: { transform: "translate3d(0,-40px,0)", opacity: 0 },
-    enter: { transform: "translate3d(0,0px,0)", opacity: 1, delay: 800 },
-    leave: {
-      transform: "translate3d(0,-40px,0)",
-      opacity: 0,
-    },
-    config: config.slow,
-    onDestroyed: () => callback(destination),
-  })
 
   const handleClick = dest => {
     setDestination(dest)
     setShow(false)
   }
 
-  return transitions.map(
-    ({ item, key, props }) =>
-      item && (
+  const transition = useTransition(
+    <>
+      <Col x>
+        <TitlePrimary>Isaac Z Tai</TitlePrimary>
+        <Caption
+          css={css`
+            cursor: pointer;
+            pointer-events: all;
+            &:hover {
+              text-decoration: underline;
+            }
+          `}
+          onClick={() => handleClick("/")}
+        >
+          this is too much
+        </Caption>
+        <Row
+          y
+          space="around"
+          css={css`
+            align-self: stretch;
+          `}
+        >
+          <SquareIcon url={require("../images/LinkedinLogo.png")} />
+          <SquareIcon url={require("../images/GitHubLogo.png")} />
+        </Row>
+      </Col>
+      <Orbit radius={`445px`} duration={60} delay={30} reverse={true}>
+        <Title2 onClick={() => handleClick("/contact")}>Contact</Title2>
+      </Orbit>
+      <Orbit radius={`525px`} duration={90} delay={30}>
+        <Title2 onClick={() => handleClick("/mobile")}>Mobile</Title2>
+      </Orbit>
+      <Orbit radius={`600px`} duration={50} delay={300} reverse={true}>
+        <Title2 onClick={() => handleClick("/web")}>Web</Title2>
+      </Orbit>
+      <Orbit radius={`675px`} duration={40} delay={420}>
+        <Title2 onClick={() => handleClick("/other")}>Other</Title2>
+      </Orbit>
+    </>,
+    {
+      from: { transform: "translate3d(0,-40px,0)", opacity: 0 },
+      enter: { transform: "translate3d(0,0px,0)", opacity: 1, delay: 800 },
+      leave: {
+        // transform: "translate3d(0,-40px,0)",
+        opacity: 0,
+        // onRest: () => callback(destination),
+      },
+      config: config.slow,
+    }
+  )
+
+  return transition(
+    (values, item) =>
+      show && (
         <a.div
           css={css`
             position: relative;
@@ -63,35 +105,9 @@ const AnimatedContent = ({ callback = () => {}, onClick }) => {
             align-items: center;
             background-color: transparent;
           `}
-          style={props}
+          style={values}
         >
-          <Col x>
-            <TitlePrimary>Isaac Z Tai</TitlePrimary>
-            <Caption
-              css={css`
-                cursor: pointer;
-                pointer-events: all;
-                &:hover {
-                  text-decoration: underline;
-                }
-              `}
-              onClick={() => handleClick("/")}
-            >
-              this is too much
-            </Caption>
-          </Col>
-          <Orbit radius={`445px`} duration={60} delay={30} reverse={true}>
-            <Title2 onClick={() => handleClick("/contact")}>Contact</Title2>
-          </Orbit>
-          <Orbit radius={`525px`} duration={90} delay={30}>
-            <Title2 onClick={() => handleClick("/mobile")}>Mobile</Title2>
-          </Orbit>
-          <Orbit radius={`600px`} duration={50} delay={300} reverse={true}>
-            <Title2 onClick={() => handleClick("/web")}>Web</Title2>
-          </Orbit>
-          <Orbit radius={`675px`} duration={40} delay={420}>
-            <Title2 onClick={() => handleClick("/other")}>Other</Title2>
-          </Orbit>
+          {item}
         </a.div>
       )
   )
